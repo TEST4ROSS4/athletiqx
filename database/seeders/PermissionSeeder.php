4530741096248cb836fc9f89337bc7eaa1c52c15
TEST4ROSS4 <kimberlyrabusa1004@gmail.com>
2025-09-28 19:47:41 +0800

    Spatie Install and Setup
    - created permission seeder
    - added permissions and migrated it to db

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            "users.create",
+            "users.view",
+            "users.edit",
+            "users.delete",
+            "roles.create",
+            "roles.view",
+            "roles.edit",
+            "roles.delete"
+        ];
+        foreach ($permissions as $key => $value) {
+            Permission::create(["name" => $value]);
+        }
+    }
+}
